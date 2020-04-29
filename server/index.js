aebe01904796f3e2715642dcd78ca953398ed00d
NANAA <nathan.lebon@epitech.eu>
2020-04-29 06:39:22 -0400

    fixed port and start script

--- server/index.js
+++ server/index.js
@@ -3,11 +3,14 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
+const PORT = process.env.PORT || 3000;
+
+
 
 let rooms = {};
 
-server.listen(8080, () => {
-  console.log("Server listening on port 8080");
+server.listen(PORT, () => {
+  console.log("Server listening on port " + PORT);
 });
 
 io.on('connection', (socket) => {
